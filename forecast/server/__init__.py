b88092b97c282372a02c687cc4c3a404366c7e56
BlackPoint-CX <chenxiang0815@gmail.com>
2021-10-17 08:42:50 +0800

    2021-10-17

--- forecast/server/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-"""
-__author__ : BlackPoint-CX
-__email__ : msg.receiver.bp@gmail.com
-__file_name__ : __init__.py.py
-__create_time__ : 2019/11/03
-"""
