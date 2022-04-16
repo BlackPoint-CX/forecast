5b1096f6efa4e91fe76bd2a887eecbd2d320e038
BlackPoint-CX <chenxiang0815@gmail.com>
2022-04-16 20:17:06 +0800

    remove build_tools subdir.

--- build_tools/travis/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "python version:"
-python --version
-
-pip install numpy
-pip install scipy
--- build_tools/travis/test_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-python --version
-python -c "import numpy; print('numpy %s' % numpy.__version__)"
-python -c "import scipy; print('scipy %s' % scipy.__version__)"
